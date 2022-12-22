d5e7f91536d8d9a258ace29142594bb0aa265787
Brad Wilson <dotnetguy@gmail.com>
2022-12-22 15:12:22 -0800

    Day 22: Fixing data overlap bug

--- day22/Program.cs
+++ day22/Program.cs
@@ -12,6 +12,11 @@ var data =
 
 var map = data[0..(data.Length - 2)].Select(d => d.ToArray()).ToArray();
 
+const int RIGHT = 0;
+const int DOWN = 1;
+const int LEFT = 2;
+const int UP = 3;
+
 long GetPart1()
 {
 	var posX = 0;
@@ -56,7 +61,7 @@ long GetPart1()
 				var resultX = posX;
 				var resultY = posY;
 
-				if (direction == 0)
+				if (direction == RIGHT)
 				{
 					resultX++;
 					if (resultX >= map[resultY].Length)
@@ -69,7 +74,7 @@ long GetPart1()
 							resultX = 0;
 					}
 				}
-				else if (direction == 1)
+				else if (direction == DOWN)
 				{
 					resultY++;
 					if (resultY >= map.Length)
@@ -82,7 +87,7 @@ long GetPart1()
 							resultY -= map.Length;
 					}
 				}
-				else if (direction == 2)
+				else if (direction == LEFT)
 				{
 					resultX--;
 					if (resultX < 0)
@@ -95,7 +100,7 @@ long GetPart1()
 							resultX += map[resultY].Length;
 					}
 				}
-				else if (direction == 3)
+				else if (direction == UP)
 				{
 					resultY--;
 					if (resultY < 0)
@@ -125,59 +130,62 @@ long GetPart1()
 
 long GetPart2()
 {
-	var emptySpaceMaps = new Dictionary<(int x, int y), (int x, int y, int directionChange)>();
+	var emptySpaceMaps = new Dictionary<(int x, int y, int direction), (int x, int y, int directionChange)>();
 
-#if false  // Used for sample data
-	for (int i = 0; i < 4; ++i)
+	if (map.Length == 12)  // Sample data
 	{
-		// 0 left to 4 top
-		emptySpaceMaps[(7, i)] = (4 + i, 4, -1);
-		emptySpaceMaps[(4 + i, 3)] = (8, i, +1);
-		// 0 top to 3 top
-		emptySpaceMaps[(i + 8, -1)] = (3 - i, 4, -2);
-		emptySpaceMaps[(i, 3)] = (11 - i, 0, +2);
-		// 0 right to 5 right
-		emptySpaceMaps[(12, i)] = (15, 11 - i, -2);
-		emptySpaceMaps[(16, i + 8)] = (11, 3 - i, 2);
-		// 1 right to 5 top
-		emptySpaceMaps[(12, i + 4)] = (15 - i, 8, +1);
-		emptySpaceMaps[(12 + i, 7)] = (11, 7 - i, -1);
-		// 2 left to 4 bottom
-		emptySpaceMaps[(7, 8 + i)] = (7 - i, 7, +1);
-		emptySpaceMaps[(i + 4, 8)] = (11 - i, 11, -1);
-		// 2 bottom to 3 bottom
-		emptySpaceMaps[(i + 8, 12)] = (3 - i, 7, +2);
-		emptySpaceMaps[(i, 8)] = (11 - i, 11, -2);
-		// 3 left to 5 bottom
-		emptySpaceMaps[(-1, i + 4)] = (15 - i, 11, +1);
-		emptySpaceMaps[(12 + i, 12)] = (0, 7 - i, -1);
+		for (int i = 0; i < 4; ++i)
+		{
+			// 0 left to 4 top
+			emptySpaceMaps.Add((7, i, LEFT), (4 + i, 4, -1));
+			emptySpaceMaps.Add((4 + i, 3, UP), (8, i, +1));
+			// 0 top to 3 top
+			emptySpaceMaps.Add((i + 8, -1, UP), (3 - i, 4, -2));
+			emptySpaceMaps.Add((i, 3, UP), (11 - i, 0, +2));
+			// 0 right to 5 right
+			emptySpaceMaps.Add((12, i, RIGHT), (15, 11 - i, -2));
+			emptySpaceMaps.Add((16, i + 8, RIGHT), (11, 3 - i, 2));
+			// 1 right to 5 top
+			emptySpaceMaps.Add((12, i + 4, RIGHT), (15 - i, 8, +1));
+			emptySpaceMaps.Add((12 + i, 7, UP), (11, 7 - i, -1));
+			// 2 left to 4 bottom
+			emptySpaceMaps.Add((7, 8 + i, LEFT), (7 - i, 7, +1));
+			emptySpaceMaps.Add((i + 4, 8, DOWN), (11 - i, 11, -1));
+			// 2 bottom to 3 bottom
+			emptySpaceMaps.Add((i + 8, 12, DOWN), (3 - i, 7, +2));
+			emptySpaceMaps.Add((i, 8, DOWN), (11 - i, 11, -2));
+			// 3 left to 5 bottom
+			emptySpaceMaps.Add((-1, i + 4, LEFT), (15 - i, 11, +1));
+			emptySpaceMaps.Add((12 + i, 12, DOWN), (0, 7 - i, -1));
+		}
 	}
-#else  // Used for real data, and it's specific to my data, so it's not a general purpose solution
-	for (int i = 0; i < 50; ++i)
+	else  // The real data -- hand crafted for our known data set (not general purpose)
 	{
-		// 1 top to 6 left (up to right)
-		emptySpaceMaps[(50 + i, -1)] = (0, 150 + i, +1);
-		emptySpaceMaps[(-1, 150 + i)] = (50 + i, 0, -1);
-		// 1 left to 5 left (left to right)
-		emptySpaceMaps[(49, i)] = (0, 149 - i, 2);
-		emptySpaceMaps[(-1, 100 + i)] = (50, 49 - i, -2);
-		// 2 top to 6 bottom (up to up)
-		emptySpaceMaps[(100 + i, -1)] = (i, 199, 0);
-		emptySpaceMaps[(i, 200)] = (100 + i, 0, 0);
-		// 2 right to 4 right (right to left)
-		emptySpaceMaps[(150, i)] = (99, 149 - i, 2);
-		emptySpaceMaps[(100, 100 + i)] = (149, 49 - i, -2);
-		// 2 bottom to 3 right (down to left)
-		emptySpaceMaps[(100 + i, 50)] = (99, 50 + i, +1);
-		emptySpaceMaps[(100, 50 + i)] = (100 + i, 49, -1);
-		// 3 left to 5 top (left to down)
-		emptySpaceMaps[(49, 50 + i)] = (i, 100, -1);
-		emptySpaceMaps[(i, 99)] = (50, 50 + i, +1);
-		// 4 bottom to 6 right (down to left)
-		emptySpaceMaps[(50 + i, 150)] = (49, 150 + i, +1);
-		emptySpaceMaps[(50, 150 + i)] = (50 + i, 149, -1);
+		for (int i = 0; i < 50; ++i)
+		{
+			// 1 top to 6 left
+			emptySpaceMaps.Add((50 + i, -1, UP), (0, 150 + i, +1));
+			emptySpaceMaps.Add((-1, 150 + i, LEFT), (50 + i, 0, -1));
+			// 1 left to 5 left
+			emptySpaceMaps.Add((49, i, LEFT), (0, 149 - i, 2));
+			emptySpaceMaps.Add((-1, 100 + i, LEFT), (50, 49 - i, -2));
+			// 2 top to 6 bottom
+			emptySpaceMaps.Add((100 + i, -1, UP), (i, 199, 0));
+			emptySpaceMaps.Add((i, 200, DOWN), (100 + i, 0, 0));
+			// 2 right to 4 right
+			emptySpaceMaps.Add((150, i, RIGHT), (99, 149 - i, 2));
+			emptySpaceMaps.Add((100, 100 + i, RIGHT), (149, 49 - i, -2));
+			// 2 bottom to 3 right
+			emptySpaceMaps.Add((100 + i, 50, DOWN), (99, 50 + i, +1));
+			emptySpaceMaps.Add((100, 50 + i, RIGHT), (100 + i, 49, -1));
+			// 3 left to 5 top
+			emptySpaceMaps.Add((49, 50 + i, LEFT), (i, 100, -1));
+			emptySpaceMaps.Add((i, 99, UP), (50, 50 + i, +1));
+			// 4 bottom to 6 right
+			emptySpaceMaps.Add((50 + i, 150, DOWN), (49, 150 + i, +1));
+			emptySpaceMaps.Add((50, 150 + i, RIGHT), (50 + i, 149, -1));
+		}
 	}
-#endif
 
 	var posX = 0;
 	var posY = 0;
@@ -232,7 +240,7 @@ long GetPart2()
 				else if (direction == 3)
 					resultY--;
 
-				if (emptySpaceMaps.TryGetValue((resultX, resultY), out var nextSpace))
+				if (emptySpaceMaps.TryGetValue((resultX, resultY, direction), out var nextSpace))
 					(resultX, resultY, directionChange) = nextSpace;
 
 				if (map[resultY][resultX] != '#')
